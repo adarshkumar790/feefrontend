0e82e5626cc4994458389e221eef3f9d6f9421a0
AdarhKumar <adarshnit420@gmail.com>
2024-09-21 10:37:28 +0530

    first commit

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -1,42 +1,33 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-// import './style.css';  // Import CSS file
-
-// function StudentList({ students }) {
-//   return (
-//     <ul className="student-list">
-//       {students.map(student => (
-//         <li key={student._id} className="student-list-item">
-//           {student.name} - {student.class} - Roll No: {student.rollNo}
-//           <Link to={`/fee/${student._id}`} className="view-fee-link">
-//             View Fee Details
-//           </Link>
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// }
-
-// export default StudentList;
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './style.css';  // Import CSS file
 
 function StudentList({ students }) {
   return (
-    <ul className="student-list">
-      {students.map(student => (
-        <li key={student._id} className="student-list-item">
-          {student.name} - {student.class} - Roll No: {student.rollNo}
-          <Link to={`/fee/${student._id}`} state={{ 
-            name: student.name,
-            rollNo: student.rollNo
-          }} className="view-fee-link">
-            View Fee Details
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <>
+    
+    <div>
+      
+      <ul className="student-list">
+        {students.map(student => (
+          <li key={student._id} className="student-list-item">
+            {student.name} - {student.class} - Roll No: {student.rollNo}
+            <Link to={`/fee/${student._id}`} state={{ 
+              name: student.name,
+              rollNo: student.rollNo
+            }} className="view-fee-link">
+              View Fee Details
+            </Link>
+          </li>
+        ))}
+      </ul>
+      
+      {/* <div className="button-container">
+        <Link to="/add-student" className="add-student-button">Add Student</Link>
+        <Link to="/generate-receipt" className="generate-receipt-button">Generate Receipt</Link>
+      </div> */}
+    </div>
+    </>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,10 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import StudentList from '../components/StudentList';
 import FeeCollection from '../components/FeeCollection';
+import { Link } from 'react-router-dom';
 import './style.css';  // Import CSS file
 const baseURL = 'https://feebackend.onrender.com'
+
 
 function Home() {
   const [students, setStudents] = useState([]);
@@ -20,6 +22,10 @@ function Home() {
   return (
     <div className="home-container">
       <h1>Student List</h1>
+      <div className="button-container">
+        <Link to="/add-student" className="add-student-button">Add Student</Link>
+        <Link to="/receipt" className="generate-receipt-button">Generate Receipt</Link>
+      </div>
       <div className="filter-container">
         <label>Filter by Session:</label>
         <input 
